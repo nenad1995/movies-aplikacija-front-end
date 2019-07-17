5971e19ba9b5737c9ae9d793303966db76d356d9
nenad1995 <nenad.zoric@yahoo.com>
2019-07-17 21:11:05 +0200

    Zadatak 2

---
+++ src/services/movies.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export class MoviesService({
+  constructor() {
+    axios.defaults.baseURL = 'http://localhost:3000/api/'
+  }
+})
+
+export const moviesService = new MoviesService()
